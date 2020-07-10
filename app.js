2272341f11bf45fc2fa8c3b878e1547a2e85fb5a
Alejandro Abad <alejandro.abad.noriega@gmail.com>
2020-07-10 17:55:00 +0200

    un fix

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-const jsonServer = require("json-server");
-const morgan = require("morgan");
-
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const options = { noCors: true }; // Disables CORS
+const middlewares = jsonServer.defaults(options);
+
 const PORT = process.env.PORT;
 
 server.use(middlewares);
 server.use(morgan("dev"));
-server.use((req, res, next) => { // Middleware to disable CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-})
 server.use(router);
 
 server.listen(PORT, () => {
